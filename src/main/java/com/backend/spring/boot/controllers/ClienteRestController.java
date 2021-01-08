92ddd0295d0eaad960d0257669551033159dc30c
Alejandro Bernal Castiblanco <albernalca@unal.edu.co>
2021-01-08 17:22:37 -0500

    Agrego CrossOrigin y la ruta del FrontEnd en el package controllers - ClienteRestController

--- src/main/java/com/backend/spring/boot/controllers/ClienteRestController.java
+++ src/main/java/com/backend/spring/boot/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.backend.spring.boot.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.backend.spring.boot.models.entity.Cliente;
 import com.backend.spring.boot.models.services.IClienteService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
