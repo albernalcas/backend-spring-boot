c842824d9726093cfc5d56a4cfc3c6f1a834ad41
Alejandro Bernal Castiblanco <albernalca@unal.edu.co>
2021-01-09 19:56:49 -0500

    Creo los métodos CRUD en la clase ClienteServiceImpl

--- src/main/java/com/backend/spring/boot/models/services/IClienteService.java
+++ src/main/java/com/backend/spring/boot/models/services/IClienteService.java
@@ -5,7 +5,17 @@ import java.util.List;
 import com.backend.spring.boot.models.entity.Cliente;
 
 public interface IClienteService {
-
+	
+	
 	public List<Cliente> findAll();
+	
+	
+	public Cliente findbyId(Long id);
+	
+	
+	public Cliente save(Cliente cliente);
+	
+
+	public void delete (Long id);
 
 }
